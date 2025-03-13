979a69bd47d1472582d231dd2eb7822bec40e5f8
NomaTyx <jjziebart99@gmail.com>
2025-03-13 00:09:20 -0700

    validParentheses (learned how to use a stack)

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,39 @@
+﻿//leetcode easy
+//TIL what a stack is
+bool IsValid(string s) {
+    Stack<char> chars = new Stack<char>();
+
+    foreach (char c in s) {
+        switch(c) {
+            default:
+                chars.Push(c);
+                break;
+            case ')':
+                if(chars.Count == 0) return false;
+                if(chars.Peek() == '(') {
+                    chars.Pop();
+                } else {
+                    chars.Push(c);
+                }
+                break;
+            case '}':
+                if(chars.Count == 0) return false;
+                if(chars.Peek() == '{') {
+                    chars.Pop();
+                } else {
+                    chars.Push(c);
+                }
+                break;
+            case ']':
+                if(chars.Count == 0) return false;
+                if(chars.Peek() == '[') {
+                    chars.Pop();
+                } else {
+                    chars.Push(c);
+                }
+                break;
+        }
+    }
+
+    return chars.Count == 0;
+}
