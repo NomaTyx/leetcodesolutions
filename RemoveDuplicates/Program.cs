4f9fa870b986350347c5a11d3c52db0378c7a045
NomaTyx <jjziebart99@gmail.com>
2025-03-13 01:47:25 -0700

    removeduplicates

---
+++ RemoveDuplicates/Program.cs
@@ -0,0 +1,26 @@
+﻿//not the most proud of this one. ugh. tired. also i use lists more than arrays so definitely something to work on in the future.
+
+int RemoveDuplicates(int[] nums)
+{
+    int k = nums.Length;
+    int temp;
+
+    for (int i = 0; i < k - 1; i++)
+    {
+        if (k == 1) return k;
+        if (nums[i] == nums[i + 1])
+        {
+            for (int j = i; j < nums.Length - 1; j++)
+            {
+                temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+            }
+
+            k--;
+            i--;
+        }
+    }
+    return k;
+}
+Console.WriteLine(RemoveDuplicates([1, 2, 3, 4, 5, 6]));
