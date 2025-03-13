90f5492d95c962bed1594ec8b823656d4d17e087
NomaTyx <jjziebart99@gmail.com>
2025-03-13 01:04:24 -0700

    merge two linked lists

---
+++ MergeTwoLinkedLists/Program.cs
@@ -0,0 +1,35 @@
+﻿//leetcode easy
+//this is sorta my first foray into linked lists. Didn't go too poorly, I didn't have to copy a tutorial verbatim, I was able to hear concepts and extrapolate on my own. 
+
+ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+    ListNode temp = new ListNode();
+    ListNode newList = temp;
+
+    while (list1 != null && list2 != null) {
+        if(list1.val <= list2.val) {
+            temp.next = list1;
+            list1 = list1.next;
+        } else {
+            temp.next = list2;
+            list2 = list2.next;
+        }
+        temp = temp.next;
+    }
+
+    if(list1 != null) {
+        temp.next = list1;
+    } else {
+        temp.next = list2;
+    }
+
+    return newList.next;
+}
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
