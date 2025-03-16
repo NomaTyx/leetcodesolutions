087d1b48339ff49a4fd940bcda144269f31343af
NomaTyx <jjziebart99@gmail.com>
2025-03-16 14:23:19 -0700

    did mysqrt. someone gave me the idea to use a binary search. i don't search for elements ever-- i don't do that in the games ive made, inventories and somesuch are hardcoded to be small enough to just iterate through

---
+++ MySqrt/Program.cs
@@ -0,0 +1,30 @@
+﻿int MySqrt(int x)
+{
+    //46341 is sqrt(max_value)
+    int min = 0, max = 46341;
+    int mid = min + max / 2;
+
+    do
+    {
+        if (mid * mid < x)
+        {
+            min = mid;
+        }
+        else if (mid * mid > x)
+        {
+            max = mid;
+        }
+        else
+        {
+            return mid;
+        }
+
+        //note to self when writing binary searches: this goes on the bottom and not on the top!!
+        mid = (min + max) / 2;
+    } while (mid > min && mid < max);
+
+    return mid;
+}
+
+
+Console.WriteLine(MySqrt(2));
